a92b39ae2df68d21a6f949fa2accab2a92dc60f5
MANASCHAI YODSIN <manaschaiy@pccth.com>
2026-07-08 10:31:58 +0700

    Improve Firebase auth: use initializeAuth with browserLocalPersistence and add debug logging

--- orange-lash-app/src/firebase.js
+++ orange-lash-app/src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, initializeAuth, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -13,6 +13,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: browserLocalPersistence
+});
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 export const db = getFirestore(app);
